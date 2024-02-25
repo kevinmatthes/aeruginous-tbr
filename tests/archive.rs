8101fbcdda2e8f0f648c887cdd6b7c97219acc24
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2024-02-25 15:26:34 +0100

    Added ::= TAR:  handle symlinks correctly; fixes #39

--- tests/archive.rs
+++ tests/archive.rs
@@ -20,7 +20,7 @@
 mod tar {
     use aeruginous_io::PathBufLikeReader;
     use aeruginous_tbr::Tar;
-    use std::path::PathBuf;
+    use std::{os::unix::fs::symlink, path::PathBuf};
     use tempfile::tempdir;
 
     #[test]
@@ -67,7 +67,7 @@ mod tar {
         let d = d.path().to_str().unwrap();
         let tar = Tar::new(d.to_string() + "/archive.tar");
 
-        std::os::unix::fs::symlink(
+        symlink(
             "does_not_exist.txt",
             d.to_string() + "/no_such.txt",
         )
@@ -77,7 +77,18 @@ mod tar {
             .add_files(&["LICENSE".to_string(), d.to_string() + "/no_such.txt"])
             .is_ok());
         assert_eq!(tar.list().unwrap(), [PathBuf::from("LICENSE")]);
-        assert!(tar.remove().is_ok());
+    }
+
+    #[test]
+    fn symlink_exists() {
+        let d = tempdir().unwrap();
+        let d = d.path().to_str().unwrap();
+        let tar = Tar::new(d.to_string() + "/archive.tar");
+
+        symlink("CITATION.cff", d.to_string() + "/main.cff").unwrap();
+
+        assert!(tar.add_files(&["LICENSE", d.to_string() + "/main.cff"]).is_ok());
+        assert_eq!(tar.list().unwrap(), [PathBuf::from("LICENSE"), PathBuf::from("CITATION.cff")]);
     }
 
     #[test]
